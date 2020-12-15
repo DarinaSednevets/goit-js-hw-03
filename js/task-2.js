6310bd768b0f80586b5e90b80e34e9cfa76a5e50
DarinaSednevets <dariaausten@gmail.com>
2020-12-15 19:38:34 +0200

    add homework 3 js

---
+++ js/task-2.js
@@ -0,0 +1,15 @@
+const countProps = function(obj) {
+  const property = Object.entries(obj);
+  return property.length;
+
+};
+
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
